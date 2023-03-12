fc2a997c6aaf9618c0d4f9ac4071d36643ea6e5f
SarikabadayiCemBerk <120092745+SarikabadayiCemBerk@users.noreply.github.com>
2023-03-12 17:48:49 +0300

    Update main1234.java

--- main1234.java
+++ main1234.java
@@ -1,36 +1,44 @@
-import java.awt.Font;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        DataHub data = new DataHub();
+        while (args.length > 0) {
+            if(args[0].toLowerCase().equals("help")){
+                System.out.println("Problem description is given in the help.txt...");
+                showHelpMessage();
+                break;
+            }
+            if(args[0].toLowerCase().equals("generatedata")){
+                System.out.println("Problem data is shown in the GeneratedData.txt...");
+                data.output();
+                break;
+            }
+            else {
+                System.out.println(" To get more information about our problem, you can type the following commands; help or generatedata." );
+                System.out.println(" Help-for business problem , Generatedata-for viewing randomly generated output." );
+                break;
+            }
+        }
 
-public class Main1234 {
-	public static void main(String[] args) throws IOException {
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter a command:");
-		System.out.println("(Please write 'help' to read our business problem definition)");
-		String name = scanner.nextLine();
-			
-    			JFrame frame = new JFrame("Business Problem Definition");
-    			Font font = new Font(null,Font.PLAIN,8);
-    			
-    			JLabel label1 = new JLabel("In this project, we are faced with the task scheduling problem between forklifts, jets and RT machines. Each type of equipment is capable of performing certain types of tasks and they all need to be recharged throughout the day.");
-    			JLabel label2 = new JLabel("However, charging stations are limited, so we have to think carefully about when each piece of equipment needs to be charged to ensure they stay powered throughout the day. Our goal is to maximize the number of completed tasks.");
-    			
-    			label1.setFont(font);
-    			label2.setFont(font);	
-    			label1.setBounds(0, -430, 1500,900);
-    			label2.setBounds(0, -415, 1500,900);
-    			label1.setFont(new Font(null,Font.PLAIN,12));
-    			label2.setFont(new Font(null,Font.PLAIN,12));
-    			
-    			frame.add(label1);
-    			frame.add(label2);
-    			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    			frame.setSize(1400,200);
-    			frame.setLayout(null);
-    			frame.setVisible(true); 
-}
-}
+    }
+    private static void showHelpMessage() {
+        File file = new File("help.txt");
 
+        try {
+
+            Scanner scanner = new Scanner(file);
+
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+    }
+ }
